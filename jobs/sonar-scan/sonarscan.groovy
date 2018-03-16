4e3e197ecfcf8676c4c9cddfa77a7633903754e5
dhiraj kadam <dhiraj@flux7.com>
2018-03-16 12:06:45 +0530

    updating jobs

--- jobs/sonar-scan/sonarscan.groovy
+++ jobs/sonar-scan/sonarscan.groovy
@@ -1,4 +1,4 @@
-git_url = GIT_URL
+git_url = "https://github.com/dhiraj4191/sample-sonar-repo.git"
 
 job_path = "${new File(__FILE__).parent}"
 jenkinsfile = job_path + "/Jenkinsfile"
@@ -6,7 +6,7 @@ jenkinsfile = job_path + "/Jenkinsfile"
 pipelineJob("sonar-scan") {
 
   parameters {
-    stringParam("LIB_GIT_URL", lib_git_url, "GIT URL of jenkins libraries")
+    stringParam("GIT_URL", git_url, "GIT URL for sonar project")
   }        
 
   logRotator(numToKeep = 100)
